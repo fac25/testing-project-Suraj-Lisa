10185767f5d699db4d5d29c0c2839b2814209583
Suraj Pun <s_pun@outlook.com>
2022-07-06 19:41:48 +0100

    Delete Feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// create new list item when clicking add button
+// --------------------- Creates new list item when clicking add button ---------------------
 const addBtn = document.querySelector("button");
 
 addBtn.addEventListener("click", (e) => {
@@ -12,5 +12,38 @@ addBtn.addEventListener("click", (e) => {
     if (task == "") {
         alert("Please add some tasks");
     }
+
+// Adds "close" button to the new list item
+    let closeBtn = document.createElement("button"); 
+    let closeIcon = document.createTextNode("\u00D7"); // Close Icon
+    closeBtn.className = 'closeBtn';
+    
+    closeBtn.appendChild(closeIcon);
+    listItem.appendChild(closeBtn);
+
+// Close Button deletes new list item
+    closeBtn.onclick = function() {
+        let ele = this.parentElement;
+        ele.style.display = 'none';
+    }
+
 });
 
+// --------------------- Test Items ---------------------
+
+const listItemHome = document.getElementsByTagName("li"); // Adds "close" Button
+for (let i = 0; i <listItemHome.length; i++) {
+    let closeBtn = document.createElement("button");
+    let closeIcon = document.createTextNode("\u00D7");
+    closeBtn.className = 'closeBtn';
+    closeBtn.appendChild(closeIcon);
+    listItemHome[i].appendChild(closeBtn);
+}
+
+const close = document.querySelectorAll('.closeBtn'); // Close Button Deletes Item
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let ele = this.parentElement;
+        ele.style.display = 'none';
+    }
+}
