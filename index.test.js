2d47e9a87f526b15c3b5391866ebb7bd03d97007
lisahns <lisa.hanes@hotmail.de>
2022-07-07 16:52:18 +0200

    testing adding items to list

--- index.test.js
+++ index.test.js
@@ -7,6 +7,14 @@ test("Testing Shouls Pass", () => {
 })
 
 
-// test("Submitting a new task adds it to the list", () => {
-//      // test goes here 
-//     });
+test("Submitting a new task adds it to the list", () => {
+     const input = document.querySelector("input");
+     const button = document.querySelector("button");
+     input.value = "Write tests";
+     button.click();
+     const list = document.querySelector("li");
+     console.log(list);
+     equal (list.textContent, input.value + "\u00D7");
+     //x is appended symbol for deleting items of list
+     list.remove();
+    });
